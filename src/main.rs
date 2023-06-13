8104fa04e1794760ca24dac66e21748bd11aa0a6
Marijn Suijten <marijns95@gmail.com>
2023-06-13 23:09:37 +0200

    Flip a second white buffer and wait for the vblank

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use drm::buffer::{Buffer, DrmFourcc};
 use drm::control::atomic::AtomicModeReq;
 use drm::control::property::Value;
 use drm::control::{AtomicCommitFlags, Device as ControlDevice, PageFlipEvent, PageFlipFlags};
-use drm::Device as BasicDevice;
+use drm::{Device as BasicDevice, VblankWaitFlags};
 
 struct Card(File);
 
@@ -132,23 +132,67 @@ fn main() -> Result<()> {
             .add_framebuffer(&db, 32, 32)
             .context("Add framebuffer")?;
 
+        let mut db2 = card
+            .create_dumb_buffer((w as _, h as _), DrmFourcc::Rgba8888, 32)
+            .context("Create dumb buffer")?;
+
+        {
+            let mut mapping = card.map_dumb_buffer(&mut db2)?;
+            mapping.as_mut().fill(255);
+        }
+        let fb2 = card
+            .add_framebuffer(&db2, 32, 32)
+            .context("Add framebuffer")?;
+
         card.set_property(crtc, ctm, matrix.into())
             .context("Set CTM property")?;
+
+        // Note that we're never setting fb2, but we're flipping to it :)
+        card.set_crtc(crtc, Some(fb), (0, 0), &[con], Some(mode))
+            .context("Set CRTC")?;
 
         for i in 0..100 {
             let start = Instant::now();
 
-            card.set_crtc(crtc, Some(fb), (0, 0), &[con], Some(mode))
-                .context("Set CRTC")?;
+            card.page_flip(
+                crtc,
+                if i % 2 == 1 { fb2 } else { fb },
+                // Generate PageFlip events so that we don't present
+                // when a present is in flight (gives EBUSY)
+                PageFlipFlags::EVENT,
+                None,
+                // Some(drm::control::PageFlipTarget::Relative(0)),
+            )
+            .context("Page flip")?;
 
-            card.page_flip(crtc, fb, PageFlipFlags::empty(), None)
-                .context("Page flip")?;
-            // std::thread::sleep(Duration::from_millis(100));
+            let ev = card.receive_events()?;
+            for ev in ev {
+                match ev {
+                    drm::control::Event::Vblank(_) => println!("Vblank"),
+                    drm::control::Event::PageFlip(_) => println!("PageFlip"),
+                    drm::control::Event::Unknown(_) => println!("Unknown"),
+                }
+            }
+
+            // card.wait_vblank(
+            //     drm::VblankWaitTarget::Relative(0),
+            //     VblankWaitFlags::empty(),
+            //     crtc.into(),
+            //     0,
+            // )
+            // .context("Wait vblank")?;
+
             println!("Flip took {:?}", start.elapsed());
         }
 
         card.destroy_property_blob(matrix.into())
             .context("Destroy property blob")?;
+
+        card.destroy_framebuffer(fb2)
+            .context("Destroy framebuffer")?;
+
+        card.destroy_dumb_buffer(db2)
+            .context("Destroy dumb buffer")?;
 
         card.destroy_framebuffer(fb)
             .context("Destroy framebuffer")?;
