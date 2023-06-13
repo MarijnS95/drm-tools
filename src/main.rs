8da2ecbccbd948ad37f9b9dbf05b20888a6d8a9a
Marijn Suijten <marijns95@gmail.com>
2023-06-13 23:41:23 +0200

    Use atomic commit to set the CTM and page-flip at the same time

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,10 @@ use std::time::{Duration, Instant};
 use anyhow::{Context, Result};
 use drm::buffer::{Buffer, DrmFourcc};
 use drm::control::atomic::AtomicModeReq;
-use drm::control::property::Value;
-use drm::control::{AtomicCommitFlags, Device as ControlDevice, PageFlipEvent, PageFlipFlags};
+use drm::control::property::{self, Value};
+use drm::control::{
+    AtomicCommitFlags, Device as ControlDevice, PageFlipEvent, PageFlipFlags, ResourceHandle,
+};
 use drm::{Device as BasicDevice, VblankWaitFlags};
 
 struct Card(File);
@@ -56,6 +58,22 @@ fn main() -> Result<()> {
 
         println!("CRTC {}: {:#?}", i, info);
 
+        fn prop(
+            card: &impl ControlDevice,
+            hnd: impl ResourceHandle,
+            name: &str,
+        ) -> property::Handle {
+            card.get_properties(hnd)
+                .unwrap()
+                .iter()
+                .find(|(&phnd, _value)| {
+                    let info = card.get_property(phnd).unwrap();
+                    info.name().to_string_lossy() == name
+                })
+                .map(|(&h, _)| h)
+                .expect(name)
+        };
+
         let props = card.get_properties(crtc)?;
 
         let (&active, &active_val) = props
@@ -72,7 +90,8 @@ fn main() -> Result<()> {
         // for i in 0..5 {
         //     // card.set_property(crtc, active, 0)?;
         //     let mut req = AtomicModeReq::new();
-        //     req.add_property(crtc, active, Value::Boolean(i % 2 == 0));
+        //     // req.add_property(crtc, active, Value::Boolean(i % 2 == 0));
+        //     req.add_raw_property(crtc.into(), active, i % 2);
         //     card.atomic_commit(AtomicCommitFlags::ALLOW_MODESET, req)?;
 
         //     std::thread::sleep(Duration::from_millis(100));
@@ -99,8 +118,10 @@ fn main() -> Result<()> {
 
         let (w, h) = mode.size();
 
+        const FORMAT: DrmFourcc = DrmFourcc::Xrgb8888;
+
         let mut db = card
-            .create_dumb_buffer((w as _, h as _), DrmFourcc::Rgba8888, 32)
+            .create_dumb_buffer((w as _, h as _), FORMAT, 32)
             .context("Create dumb buffer")?;
 
         let mut mapping = card.map_dumb_buffer(&mut db)?;
@@ -124,11 +145,11 @@ fn main() -> Result<()> {
         drop(mapping);
 
         let fb = card
-            .add_framebuffer(&db, 32, 32)
+            .add_framebuffer(&db, 24, 32)
             .context("Add framebuffer")?;
 
         let mut db2 = card
-            .create_dumb_buffer((w as _, h as _), DrmFourcc::Rgba8888, 32)
+            .create_dumb_buffer((w as _, h as _), FORMAT, 32)
             .context("Create dumb buffer")?;
 
         {
@@ -136,16 +157,31 @@ fn main() -> Result<()> {
             mapping.as_mut().fill(255);
         }
         let fb2 = card
-            .add_framebuffer(&db2, 32, 32)
+            .add_framebuffer(&db2, 24, 32)
             .context("Add framebuffer")?;
 
         // Note that we're never setting fb2, but we're flipping to it :)
-        card.set_crtc(crtc, Some(fb), (0, 0), &[con], Some(mode))
-            .context("Set CRTC")?;
+        // card.set_crtc(crtc, Some(fb), (0, 0), &[con], Some(mode))
+        //     .context("Set CRTC")?;
+
+        let &plane = card
+            .plane_handles()?
+            .iter()
+            .find(|&&handle| {
+                let plane = card.get_plane(handle).unwrap();
+                dbg!(&plane);
+
+                res_handles
+                    .filter_crtcs(plane.possible_crtcs())
+                    .contains(&crtc)
+                    && plane.formats().contains(&(FORMAT as u32))
+            })
+            .context("No possible plane")?;
 
         for i in 0..100 {
             let start = Instant::now();
 
+            let mut req = AtomicModeReq::new();
             let matrix = [
                 one_s31_32,
                 i as u64 * one_s31_32 / 100,
@@ -161,31 +197,45 @@ fn main() -> Result<()> {
                 .create_property_blob(&matrix)
                 .context("Create property blob")?;
 
-            card.set_property(crtc, ctm, matrix.into())
-                .context("Set CTM property")?;
+            req.add_property(crtc, ctm, matrix);
+
+            req.add_property(con, prop(&card, con, "CRTC_ID"), Value::CRTC(Some(crtc)));
+            // req.add_property(crtc, prop("MODE_ID"), value);
+            req.add_property(crtc, prop(&card, crtc, "ACTIVE"), Value::Boolean(true));
+            req.add_property(
+                plane,
+                prop(&card, plane, "FB_ID"),
+                Value::Framebuffer(Some(if i % 2 == 1 { fb2 } else { fb })),
+            );
+
+            card.atomic_commit(
+                // If not setting NONBLOCK, this waits on the vsync signal for us
+                AtomicCommitFlags::ALLOW_MODESET, /* | AtomicCommitFlags::PAGE_FLIP_EVENT */
+                req,
+            )?;
 
             card.destroy_property_blob(matrix.into())
                 .context("Destroy property blob")?;
 
-            card.page_flip(
-                crtc,
-                if i % 2 == 1 { fb2 } else { fb },
-                // Generate PageFlip events so that we don't present
-                // when a present is in flight (gives EBUSY)
-                PageFlipFlags::EVENT,
-                None,
-                // Some(drm::control::PageFlipTarget::Relative(0)),
-            )
-            .context("Page flip")?;
+            // card.page_flip(
+            //     crtc,
+            //     if i % 2 == 1 { fb2 } else { fb },
+            //     // Generate PageFlip events so that we don't present
+            //     // when a present is in flight (gives EBUSY)
+            //     PageFlipFlags::EVENT,
+            //     None,
+            //     // Some(drm::control::PageFlipTarget::Relative(0)),
+            // )
+            // .context("Page flip")?;
 
-            let ev = card.receive_events()?;
-            for ev in ev {
-                match ev {
-                    drm::control::Event::Vblank(_) => println!("Vblank"),
-                    drm::control::Event::PageFlip(_) => println!("PageFlip"),
-                    drm::control::Event::Unknown(_) => println!("Unknown"),
-                }
-            }
+            // let ev = card.receive_events()?;
+            // for ev in ev {
+            //     match ev {
+            //         drm::control::Event::Vblank(_) => println!("Vblank"),
+            //         drm::control::Event::PageFlip(_) => println!("PageFlip"),
+            //         drm::control::Event::Unknown(_) => println!("Unknown"),
+            //     }
+            // }
 
             // card.wait_vblank(
             //     drm::VblankWaitTarget::Relative(0),
